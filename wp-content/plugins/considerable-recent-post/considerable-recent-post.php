df3020d182b16bd263ad230b1b9dcfbdffd9694d
Robin Pahlman <robin.pahlman@gmail.com>
2018-05-12 12:11:04 -0400

    update css

--- wp-content/plugins/considerable-recent-post/considerable-recent-post.php
+++ wp-content/plugins/considerable-recent-post/considerable-recent-post.php
@@ -14,8 +14,16 @@ function recent_post() {
   $recent_posts = wp_get_recent_posts( $args );
     foreach( $recent_posts as $recent ){
       $image = 'https://i.imgur.com/fniSFQ0.jpg';
+      $author = get_the_author();
       $categories = get_the_category($recent["ID"]);
-      return '<div style="border: 1px solid red;"><img class="card-image" src="'. $image .'">' . '<div class="card-category"><br />' . $categories[0]->name . '</div><br />' . '<a href="' . get_permalink($recent["ID"]) . '">' .   ( __($recent["post_title"])).'</a>  </li> </div>';
+      $timestamp = human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago';
+      $author_id = the_author_link();
+
+      // return '<div class="card-container" style="border: 1px solid red;"><img class="card-image" src="'. $image .'">' . '<div class="card-category"><br />' . $categories[0]->name . '</div><br />' . '<div class="post-title"><a href="' . get_permalink($recent["ID"]) . '">' .   ( __($recent["post_title"])).'</a>  </div> </div>';
+
+      // return '<a href="' . get_permalink($recent["ID"]) . '"><div class="card-container"><img class="card-image" src="'. $image .'">' . '<div class="card-text"><h4 class="card-category">' . $categories[0]->name . '</h4>' . '<h1 class="post-title">' .   ( __($recent["post_title"])). '</h1><p>By <a href="' . $author_id . '">' . $author . '</a>' . $timestamp . '</p></div></div></a>';
+
+      return '<div class="card-container"><img class="card-image" src="'. $image .'">' . '<div class="card-text"><h4 class="card-category">' . $categories[0]->name . '</h4>' . '<h1 class="post-title"><a href="' . get_permalink($recent["ID"]) . '">' .   ( __($recent["post_title"])) . '</a></h1><p>By ' . get_the_author_link() . ' ' . $timestamp . '</p></div></div></a>';
     }
 }
 
